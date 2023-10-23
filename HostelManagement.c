e8a7f6853292dc5a961fa06e263cd7289a1bfb88
morsalin101 <57835317+morsalin101@users.noreply.github.com>
2023-10-23 23:26:29 +0600

    Update HostelManagement.c

--- HostelManagement.c
+++ HostelManagement.c
@@ -2,36 +2,34 @@
 #include<stdlib.h>
 #include<string.h>
 
-typedef struct student 
+typedef struct student
 {
     char name[20];
     int id;
     char fname[20];
     char mname[20];
 }student;
-typedef struct node 
-{    
+typedef struct node
+{
     int count;
     char date[15];
     int  roomid;
     student studentDetails;
-    student *next;
+    struct node *next;
 }node;
 
 int interface();
 void roomAllocation();
 void showAllRecords();
 void searchRoom();
+void saveToFile();
+void readAndWrite();
 
-
+node *head;
 
 
 
 int main(){
-   node *firstNode;
-   firstNode = (node*)malloc(sizeof(node));
-   firstNode->count = 0;
-   firstNode->next = NULL;
    while(1){
    int choice = interface();
    if (choice == 1)
@@ -42,7 +40,7 @@ int main(){
     printf("\t\t\t\tEnter Name:");
     scanf("%s",name);
     strcpy(st.name,name);
-    printf("\t\t\t\tEnter ID:");
+    printf("\t\t\t\tEnter Student ID:");
     scanf("%d",&st.id);
     fflush(stdin);
     printf("\t\t\t\tEnter Fathers Name:");
@@ -52,24 +50,39 @@ int main(){
     printf("\t\t\t\tEnter Mothers Name:");
     scanf("%s",name);
     strcpy(st.mname,name);
-    roomAllocation(firstNode,st);
+    roomAllocation(head,st);
     printf("\n\n\t\tPress enter to go Menu.....");
     getchar();
    }
   else if (choice == 2)
   {
+    system("cls");
     printf("\t\t\t\tEnter ID:");
     int id;scanf("%d",&id);
-    searchRoom(firstNode,id);
-    
+    searchRoom(head,id);
+    fflush(stdin);
+    getchar();
+
   }
- /* else if (choice == 3)
+  else if (choice == 0)
   {
-    
-  }*/
+    system("cls");
+    saveToFile(head);
+    fflush(stdin);
+    getchar();
+  }
   else if(choice==4){
-     
-    showAllRecords(firstNode->next);
+
+    showAllRecords(head);
+  }
+ else if(choice == 9){
+    readAndWrite(head);
+    fflush(stdin);
+    getchar();
+  }
+   else if(choice==5){
+
+    exit(0);
   }
   else
   {
@@ -93,6 +106,8 @@ int interface()
   printf("\n\n\t\t\t\t2.Search Room\n");
   printf("\n\n\t\t\t\t3.Update Room Record\n\n");
   printf("\n\n\t\t\t\t4.Show All Room Records\n\n");
+  printf("\n\n\t\t\t\t0.Save to File\n\n");
+  printf("\n\n\t\t\t\t9.Read from file and write into linked List\n\n");
   printf("\n\n\t\t\t\t5.Exit\n\n");
   printf("\t\t\t\t-----------------------------------------------\n");
   printf("\t\t\t\tEnter to access:");
@@ -100,23 +115,28 @@ int interface()
   return choice1;
 }
 void roomAllocation(node *nd,student info){
+    printf("hello\n");
     node *temp;
     temp = (node*)malloc(sizeof(node));
     temp->studentDetails = info;
     temp->next = NULL;
-    nd->count++;
     printf("\t\t\t\tEnter Room ID:");
     scanf("%d",&temp->roomid);
     printf("\t\t\t\tEnter date:");
     char date[10];
     scanf("%s",date);
     strcpy(temp->date,date);
+    if(head==NULL){
+        head = temp;
+    }
+    else{
     while(nd->next!=NULL)
         nd=nd->next;
     nd->next = temp;
-   printf("\t\t\t\tRoom ID: %d is booked now ",temp->roomid);
+    }
+   printf("\t\t\t\tRoom  %d is booked now ",temp->roomid);
    fflush(stdin);
-   getchar();    
+   getchar();
 }
 
 void showAllRecords(node *nd){
@@ -125,8 +145,11 @@ void showAllRecords(node *nd){
       printf("\t\t\t\t|          ALL ROOM RECORDS             |\n");
       printf("\t\t\t\t|---------------------------------------------|\n\n");
   printf("\tName\tId\tFathers Name\tMothers Name\tRoodID\tDate\n\n");
+    printf("\t----\t--\t------------\t------------\t------\t----\n\n");
   node *t = nd;
-  while( t!=NULL){
+
+
+  while( t != NULL ){
     printf("\t%s\t%d\t%s\t\t%s\t\t%d\t%s\n",t->studentDetails.name,t->studentDetails.id,t->studentDetails.fname,t->studentDetails.mname,t->roomid,t->date);
     t = t->next;
   }
@@ -143,7 +166,7 @@ void searchRoom(node *nd,int id){
   while( t->next!=NULL){
     if(t->roomid == id){
       break;
-    }   
+    }
     t = t->next;
   }
   node *temp;
@@ -152,5 +175,43 @@ void searchRoom(node *nd,int id){
  fflush(stdin);
  getchar();
 }
+void saveToFile(node *nd){
+  FILE *fp = fopen("student.doc","w");
 
+  while(nd != NULL){
+    fwrite(nd,sizeof(node),1,fp);
+    nd = nd -> next;
+  }
+fclose(fp);
+printf("ALL DATA SAVED....\n");
+getchar();
+}
+void readAndWrite(node *nd){
+  system("cls");
+   node n1;
+   FILE *fp = fopen("student.doc","r");
+while(fread(&n1,sizeof(node),1,fp)){
+    if(nd == NULL){
+    nd = (node*)malloc(sizeof(node));
+    nd->count = n1.count;
+    strcpy(nd->date,n1.date);
+    nd->roomid = n1.roomid;
+    nd->studentDetails = n1.studentDetails;
+    nd->next = NULL;
+    head = nd;
+    }
+    else{
+    while(nd->next !=NULL) nd=nd->next;
+     nd->next = (node*)malloc(sizeof(node));
+     nd->next->count = n1.count;
+     strcpy(nd->next->date,n1.date);
+     nd->next->roomid = n1.roomid;
+     nd->next->studentDetails = n1.studentDetails;
+     nd->next->next = NULL;
+    }
+}
+fclose(fp);
+printf("LINKED LIST CREATED..\n");
+getchar();
+}
 
